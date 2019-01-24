b176adcdfe4a48a8a4dd431519f2b97d637e2a0c
rconaway-uno <41027252+rconaway-uno@users.noreply.github.com>
2019-01-23 18:23:42 -0600

    Removed Dup Line

--- polls/models.py
+++ polls/models.py
@@ -12,7 +12,6 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-    def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 		
